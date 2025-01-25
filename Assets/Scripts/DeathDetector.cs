6d22c9e7bc6853079fd36ab98c1ce4fde1c1d284
twistedPan <55745095+twistedPan@users.noreply.github.com>
2025-01-25 18:03:56 +0100

    detectors

--- Assets/Scripts/DeathDetector.cs
+++ Assets/Scripts/DeathDetector.cs
@@ -1,12 +1,16 @@
+using System;
 using UnityEngine;
 
 public class DeathDetector : MonoBehaviour
 {
+    public Action OnPlayerDied;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Player died");
+            OnPlayerDied?.Invoke();
         }
     }
 }

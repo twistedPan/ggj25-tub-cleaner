fdf1c76ce24d7dee2fb528440654d6fda05a3fc7
twistedPan <55745095+twistedPan@users.noreply.github.com>
2025-01-25 11:43:22 +0100

    please don't break

---
+++ Assets/Scripts/CarFollowCamera.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CarFollowCamera : MonoBehaviour
+{
+    public PrometeoCarController carController;
+    private Vector3 carForwartVec = Vector3.zero;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

78dc8deaa8960f9985218dfe63b7cfdd0c568b89
TheGenerativeGeneration <6620807+thegenerativegeneration@users.noreply.github.com>
2025-01-26 14:16:29 +0100

    Add Ingame menu

---
+++ Assets/MenuStuff/Scripts/Menu/InGameMenu.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    private CanvasGroup _canvasGroup;
+    private bool _menuOpen = false;
+    
+    
+    public string MainMenuSceneName;
+
+    InputAction openIngameMenu;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    public void ResetLevel()
+    {
+        // Reload the current scene
+        Debug.Log("Resetting Level");
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void LoadMainMenu()
+    {
+        // Load the main menu scene
+        Debug.Log("Loading Main Menu");
+        SceneManager.LoadScene(MainMenuSceneName);
+    }
+
+    void Start()
+    {
+        openIngameMenu = InputSystem.actions.FindAction("Open Ingame Menu");
+        _canvasGroup = GetComponent<CanvasGroup>();
+        _canvasGroup.alpha = 0;
+        _canvasGroup.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if (openIngameMenu.WasPressedThisFrame())
+        {
+            if (_menuOpen)
+            {
+                CloseMenu();
+            }
+            else
+            {
+                OpenMenu();
+            }
+    }
+
+    void OpenMenu()
+    {
+        // Open the ingame menu
+        Debug.Log("Opening Ingame Menu");
+        _canvasGroup.alpha = 1;
+        _canvasGroup.interactable = true;
+        _menuOpen = true;
+    }
+
+    void CloseMenu()
+    {
+        // Close the ingame menu
+        Debug.Log("Closing Ingame Menu");
+        _canvasGroup.alpha = 0;
+        _canvasGroup.interactable = false;
+        _menuOpen = false;
+    }
+
+}
+}
